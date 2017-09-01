668454a31edc5c66dae2500afcb093c4382ebc62
Lukasz Jedrzejewski <lukasso137@wp.pl>
2017-09-01 04:25:25 +0200

    Initial commit

---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,10 @@
+<div class="jumbotron text-center">
+  <div class="container">
+    <h1>Welcome to my website</h1>
+    <p class=lead">Welcome to my simple and temporary website! <br />
+      I made this app in PHP with Laravel and Bootstrap, also little SASS was included in this site. <br />
+      Soon, I will upload here my final project for this domain.<br />
+      The purpose of this simple app, is to show what I do, and write basic informations about me here.
+  </div>
+
+</div>
